e66f4e6056fda3df6b86a241d18df8d413e89192
Nuclear303 <67259340+Nuclear303@users.noreply.github.com>
2021-11-20 19:48:06 +0100

    shortened the file size

--- scripts/main-menu.js
+++ scripts/main-menu.js
@@ -1,24 +1,3 @@
-class Cat{
-    constructor(nazwa, base_health, base_damage,effect){
-        this.nazwa = nazwa
-        this.level = 1 
-        this.damageCalc = _=>{
-            if(this.level==1){
-                this.damage = base_damage
-            }
-            else{
-                this.damage = Math.ceil(base_damage*Math.log(this.level))+1
-            }
-        }
-        this.health = base_health+(this.level*10)
-        this.effect = effect
-    }
-}
-class Player{
-    constructor(nazwa){
-        this.nazwa=nazwa;
-    }
-}
 const mainMenu = document.querySelector("audio")
 document.querySelector("input").addEventListener("click",_=>{
     switch(document.querySelector("input").value){
