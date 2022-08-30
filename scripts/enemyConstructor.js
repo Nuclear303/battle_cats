dac93b3d903bf83f80c7958cfc299f2c934333b3
Nuclear303 <oliwier.kotlowski1@gmail.com>
2022-08-30 17:42:11 +0200

    changed tab size (4 -> 2)

--- scripts/enemyConstructor.js
+++ scripts/enemyConstructor.js
@@ -1,10 +1,10 @@
 export default class Enemy{
-    constructor(name,base_health,chapter, base_damage, color, reward) {
-        this.name=name;
-        this.chapter = chapter;  
-        this.health = base_health*this.chapter;
-        this.damage = base_damage*this.chapter;
-        this.color = color;
-        this.reward = reward*this.chapter;
-    }
+  constructor(name,base_health,chapter, base_damage, color, reward) {
+    this.name=name;
+    this.chapter = chapter;  
+    this.health = base_health*this.chapter;
+    this.damage = base_damage*this.chapter;
+    this.color = color;
+    this.reward = reward*this.chapter;
+  }
 }
