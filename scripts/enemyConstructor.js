10c96de7e282171d3132a512b6f1c848a8568eae
Nuclear303 <oliwier.kotlowski1@gmail.com>
2022-10-07 21:59:57 +0200

    Added an image propetry to the constructor

--- scripts/enemyConstructor.js
+++ scripts/enemyConstructor.js
@@ -1,10 +1,11 @@
 export default class Enemy{
-  constructor(name,base_health,chapter, base_damage, color, reward) {
+  constructor(name,base_health,chapter, base_damage, color, reward, image) {
     this.name=name;
     this.chapter = chapter;  
     this.health = base_health*this.chapter;
     this.damage = base_damage*this.chapter;
     this.color = color;
     this.reward = reward*this.chapter;
+    this.image= `../assets/${image}`;
   }
 }
