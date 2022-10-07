d149bf53950df2e4ddb8b9ad48aebaadb6022b58
Nuclear303 <oliwier.kotlowski1@gmail.com>
2022-10-07 22:03:57 +0200

    Added cat team info, may be removed later on in the project

---
+++ scripts/teams.js
@@ -0,0 +1,9 @@
+import Cat from "./catConstructor.js"
+let tank = new Cat('Tank Cat', 400, 2, "None");
+let cat = new Cat('Cat', 100,8, "None");
+let axe = new Cat('Axe Cat', 300,20,"Red");
+
+cat.health-=30
+console.log(cat.health)
+let team;
+export default team = [tank];
