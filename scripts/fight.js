82d19a44335f819d41770e23bf9f51a06fa4f54b
Nuclear303 <oliwier.kotlowski1@gmail.com>
2022-10-08 12:50:38 +0200

    Made a better enemy table, prototype

--- scripts/fight.js
+++ scripts/fight.js
@@ -3,5 +3,5 @@ let enemies = [
     new Enemy("Doge",100,1,8,"None",10,"cat.png")
 ]
 document.querySelector(".fight").addEventListener("click", _=>{
-    document.querySelector("main").innerHTML =`<figure><img src="${enemies[0].image}"><figcaption>${enemies[0].name}</figcaption></figure>`
+    document.querySelector("main").innerHTML =`<figure class="enemy"><div><img src="${enemies[0].image}"></div><span class="stats">Health:<br>${enemies[0].health}</span><span class="stats">Color:<br>${enemies[0].color}</span><figcaption>${enemies[0].name}</figcaption></figure>`
 })
