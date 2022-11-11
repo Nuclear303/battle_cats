cb250fbd70f022fd3ddd559de2571ed843bb1291
Nuclear303 <oliwier.kotlowski1@gmail.com>
2022-11-11 20:16:14 +0100

    Added new enemies, changed the view

--- scripts/fight.js
+++ scripts/fight.js
@@ -1,7 +1,10 @@
 import Enemy from "../scripts/enemyConstructor.js"
 let enemies = [
-    new Enemy("Doge",100,1,8,"None",10,"cat.png")
+    new Enemy("Doge",100,1,8,"None",10,"doge.png"),
+    new Enemy("Snache", 100,1,15,"None", 15, "Snache.png")
 ]
+
 document.querySelector(".fight").addEventListener("click", _=>{
-    document.querySelector("main").innerHTML =`<figure class="enemy"><div><img src="${enemies[0].image}"></div><span class="stats">Health:<br>${enemies[0].health}</span><span class="stats">Color:<br>${enemies[0].color}</span><figcaption>${enemies[0].name}</figcaption></figure>`
+    let rnd = Math.floor(Math.random()*enemies.length); 
+    document.querySelector("main").innerHTML =`<figure class="enemy"><div><img src="${enemies[rnd].image}"></div><span class="stats">Health:<br>${enemies[rnd].health}</span><span class="stats">Color:<br>${enemies[rnd].color}</span><figcaption>${enemies[rnd].name}</figcaption></figure>`
 })
