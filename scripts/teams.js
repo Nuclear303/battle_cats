b7f0592446c05bb80d608f2144730fbf5a671585
Nuclear303 <oliwier.kotlowski1@gmail.com>
2022-11-16 22:12:47 +0100

    Removed unnecessary debugging tools, added new ones instead

--- scripts/teams.js
+++ scripts/teams.js
@@ -1,9 +1,6 @@
 import Cat from "./catConstructor.js"
-let tank = new Cat('Tank Cat', 400, 2, "None");
-let cat = new Cat('Cat', 100,8, "None");
-let axe = new Cat('Axe Cat', 300,20,"Red");
-
-cat.health-=30
-console.log(cat.health)
+let cat = new Cat('Cat', 100,8, "None", "cat.png");
+let tank = new Cat('Tank Cat', 400, 2, "None", "tank.png");
+let axe = new Cat('Axe Cat', 300,20,"Red", "axe.png");
 let team;
-export default team = [tank];
+export default team = [cat,cat,cat,cat,cat,cat];
