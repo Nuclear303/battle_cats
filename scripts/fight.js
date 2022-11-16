764ea72b78a58b9b06fa69b8d9b9d8cce9f2bba8
Nuclear303 <oliwier.kotlowski1@gmail.com>
2022-11-16 22:10:54 +0100

    Added new enemies, created simple "Matchmaking" algorythm

--- scripts/fight.js
+++ scripts/fight.js
@@ -1,10 +1,18 @@
 import Enemy from "../scripts/enemyConstructor.js"
 let enemies = [
-    new Enemy("Doge",100,1,8,"None",10,"doge.png"),
-    new Enemy("Snache", 100,1,15,"None", 15, "Snache.png")
+    new Enemy("Doge",100,1,8,"None",10,"doge.png",false),
+    new Enemy("Snache", 100,1,15,"None", 15, "Snache.png",false),
+    new Enemy("Those Guys", 200,1,20,"None",25,"Those_Guys.png",false),
+    new Enemy("Hippoe",1000, 1, 100, "None",100,"Hippoe.png", false)
 ]
-
+let bosses = [
+]
 document.querySelector(".fight").addEventListener("click", _=>{
-    let rnd = Math.floor(Math.random()*enemies.length); 
+    let rnd = Math.floor(Math.random()*enemies.length+1); 
+    if(rnd == enemies.length){
+        rnd = Math.floor(Math.random()*bosses.length);
+        document.querySelector("main").innerHTML =`<figure class="enemy"><div style="background-color: rgba(255, 0, 0, 0.75);"><img src="${bosses[rnd].image}" ></div><span class="stats">Health:<br>${bosses[rnd].health}</span><span class="stats">Color:<br>${bosses[rnd].color}</span><figcaption>${bosses[rnd].name}</figcaption></figure>`
+        return;
+    }
     document.querySelector("main").innerHTML =`<figure class="enemy"><div><img src="${enemies[rnd].image}"></div><span class="stats">Health:<br>${enemies[rnd].health}</span><span class="stats">Color:<br>${enemies[rnd].color}</span><figcaption>${enemies[rnd].name}</figcaption></figure>`
 })
