c159ac10e197c987c561e0be641dc50478b4ed35
Nuclear303 <67259340+Nuclear303@users.noreply.github.com>
2023-04-17 07:47:32 +0200

    Update userButton.js

--- scripts/userButton.js
+++ scripts/userButton.js
@@ -1,20 +1,22 @@
-import Player from "./playerConstructor.js";
-if(localStorage.getItem("player") == undefined){
-    document.querySelector("p.user").innerHTML = "<input class='userInput' type='text' placeholder='Wpisz nazwę gracza...'><br><button class='acceptUser'>Akceptuj</button>";
-    document.querySelector("button.acceptUser").addEventListener("click",_=>{
-        const player = new Player(document.querySelector("input.userInput").value);
-        localStorage.setItem("player",player.nazwa);
-        document.querySelector("p.user").innerHTML += "<br><span style='color:green;'>&#10004; Zaakceptowano</span>";
+let user = document.querySelector("p.user");
+let acceptUser = document.querySelector("button.acceptUser");
+
+function accept(){
+    acceptUser.addEventListener("click",_=>{
+        localStorage.setItem("player",document.querySelector("input.userInput").value);
+        user.innerHTML += "<br><span style='color:green;'>&#10004; Zaakceptowano</span>"
     })
 }
+
+
+if(localStorage.getItem("player") == undefined){
+    user.innerHTML = "<input class='userInput' type='text' placeholder='Wpisz nazwę gracza...'><br><button class='acceptUser'>Akceptuj</button>";
+    accept();
+}
 else{
-    document.querySelector("p.user").innerHTML = `Witaj, ${localStorage.getItem("player")}<br><button class='changeName'>Zmień nazwę</button>`;
+    user.innerHTML = `Witaj, ${localStorage.getItem("player")}<br><button class='changeName'>Zmień nazwę</button>`;
     document.querySelector("button.changeName").addEventListener("click",_=>{
-        document.querySelector("p.user").innerHTML = "<input class='userInput' type='text' placeholder='Wpisz nazwę gracza...'><br><button class='acceptUser'>Akceptuj</button>";
-        document.querySelector("button.acceptUser").addEventListener("click",_=>{
-            const player = new Player(document.querySelector("input.userInput").value);
-            localStorage.setItem("player",player.nazwa);
-            document.querySelector("p.user").innerHTML += "<br><span style='color:green;'>&#10004; Zaakceptowano</span>"
-        })
+       user.innerHTML = "<input class='userInput' type='text' placeholder='Wpisz nazwę gracza...'><br><button class='acceptUser'>Akceptuj</button>";
+       accept();
     })
 }
