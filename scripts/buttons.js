d3491cdc484ad90308c667c621ae9130842645ae
Nuclear303 <oliwier.kotlowski1@gmail.com>
2023-04-23 20:11:50 +0200

    Added event listeners for chapter buttons

---
+++ scripts/buttons.js
@@ -0,0 +1,27 @@
+document.querySelectorAll("div.chap")[0].addEventListener("click", _=>{
+  location.assign("./chapters/empireofcats1.html");
+})
+document.querySelectorAll("div.chap")[1].addEventListener("click", _=>{
+  location.assign("./chapters/empireofcats2.html");
+})
+document.querySelectorAll("div.chap")[2].addEventListener("click", _=>{
+  location.assign("./chapters/empireofcats3.html");
+})
+document.querySelectorAll("div.chap")[3].addEventListener("click", _=>{
+  location.assign("./chapters/intothefuture1.html");
+})
+document.querySelectorAll("div.chap")[4].addEventListener("click", _=>{
+  location.assign("./chapters/intothefuture2.html");
+})
+document.querySelectorAll("div.chap")[5].addEventListener("click", _=>{
+  location.assign("./chapters/intothefuture3.html");
+})
+document.querySelectorAll("div.chap")[6].addEventListener("click", _=>{
+  location.assign("./chapters/catsofthecosmos1.html");
+})
+document.querySelectorAll("div.chap")[7].addEventListener("click", _=>{
+  location.assign("./chapters/catsofthecosmos2.html");
+})
+document.querySelectorAll("div.chap")[8].addEventListener("click", _=>{
+  location.assign("./chapters/catsofthecosmos3.html");
+})
