ea5a28170579b48f7303161e73a8c7a504fac133
Nuclear303 <67259340+Nuclear303@users.noreply.github.com>
2023-04-24 08:36:09 +0200

    Added ItF enemies

--- scripts/enemies.js
+++ scripts/enemies.js
@@ -53,10 +53,53 @@ const enemies = {
       new Enemy("Squire Rel", 80, 30, 3, "None", 30, "Squire_Rel.png", false),
       new Enemy("Teacher Bear", 3000, 1000, 3, "None", 500, "Teacher_Bear.png", true)
      ]
+  },
+  ItF:{
+    chapter1:[
+      new Enemy("Shibalien", 900, 150, 1, "Alien", 100, "Shibalien.png", false),
+      new Enemy("Hyppoh", 7000, 200, 1, "Alien", 300, "Hyppoh.png",false),
+      new Enemy("Imperator Sael", 15000, 500, 1, "Alien", 500, "Imperator_Sael.png", false),
+      new Enemy("Helmut Krabbe", 9000, 300, 1, "Alien", 400, "Helmut_Krabbe.png", false),
+      new Enemy("Kroxo", 3000, 900, 1, "Alien", 200, "Kroxo.png", false),
+      new Enemy("Ursamajor", 30000, 1000, 1, "Alien", 1000, "Ursamajor.png", true),
+      new Enemy("Elizabeth the LVIth",60000, 250, 1, "Alien", 2000, "Elizabeth_the_56th.png", true),
+      new Enemy("I.M. Phace", 44444, 1333, 1, "Alien", 1500, "IMPhace.png", true),
+      new Enemy("LeMurr", 20000, 800, 1, "Alien", 900, "LeMurr.png", false)
+    ],
+    chapter2:[
+      new Enemy("Shibalien", 900, 150, 2, "Alien", 100, "Shibalien.png", false),
+      new Enemy("Hyppoh", 7000, 200, 2, "Alien", 300, "Hyppoh.png",false),
+      new Enemy("Imperator Sael", 15000, 500, 2, "Alien", 500, "Imperator_Sael.png", false),
+      new Enemy("Helmut Krabbe", 9000, 300, 2, "Alien", 400, "Helmut_Krabbe.png", false),
+      new Enemy("Kroxo", 3000, 900, 2, "Alien", 200, "Kroxo.png", false),
+      new Enemy("Ursamajor", 30000, 1000, 2, "Alien", 1000, "Ursamajor.png", true),
+      new Enemy("Elizabeth the LVIth",60000, 250, 2, "Alien", 2000, "Elizabeth_the_56th.png", true),
+      new Enemy("I.M. Phace", 44444, 1333, 2, "Alien", 1500, "IMPhace.png", true),
+      new Enemy("LeMurr", 20000, 800, 2, "Alien", 900, "LeMurr.png", false),
+      new Enemy("Scissoroo", 24000, 2800,1 ,"Alien", 1000, "Scissoroo.png", false),
+      new Enemy("Mistress Celeboodle", 4500, 3750, 1, 1000, "Mistress Celeboodle", false)
+    ],
+    chapter3:[
+      new Enemy("Shibalien", 900, 150, 3, "Alien", 100, "Shibalien.png", false),
+      new Enemy("Hyppoh", 7000, 200, 3, "Alien", 300, "Hyppoh.png",false),
+      new Enemy("Imperator Sael", 15000, 500, 3, "Alien", 500, "Imperator_Sael.png", false),
+      new Enemy("Helmut Krabbe", 9000, 300, 3, "Alien", 400, "Helmut_Krabbe.png", false),
+      new Enemy("Kroxo", 3000, 900, 3, "Alien", 200, "Kroxo.png", false),
+      new Enemy("Ursamajor", 30000, 1000, 3, "Alien", 1000, "Ursamajor.png", true),
+      new Enemy("Elizabeth the LVIth",60000, 250, 3, "Alien", 2000, "Elizabeth_the_56th.png", true),
+      new Enemy("I.M. Phace", 44444, 1333, 3, "Alien", 1500, "IMPhace.png", true),
+      new Enemy("LeMurr", 20000, 800, 3, "Alien", 900, "LeMurr.png", false),
+      new Enemy("Calamary", 45000, 1200, 1, "Alien", 2000, "Calamary.png", false),
+      new Enemy("Alpacky", 175000, 1750, 1, "Alien", 5000, "Alpacky.png", true)
+    ]
+  },
+  CotC:{
+    chapter1:[
+      new Enemy("Shibalien Elite", 7000, 500, "Alien", 400, "Shibalien_Elite.png", false)
+    ],
+    chapter2:[],
+    chapter3:[]
   }
 }
 
-
-if(localStorage.getItem("enemies") == undefined){
-  localStorage.setItem("enemies", JSON.stringify(enemies));
-}
+localStorage.setItem("enemies", JSON.stringify(enemies));
