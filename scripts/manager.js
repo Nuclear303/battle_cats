aa32d539179efa9face2d7d8b8d6e2f8fef29a18
Nuclear303 <oliwier.kotlowski1@gmail.com>
2023-04-30 19:41:45 +0200

    Cat Team Manager with storage and team

---
+++ scripts/manager.js
@@ -0,0 +1,70 @@
+import {save} from "./save.js"
+const main = document.querySelector("main");
+let storage = JSON.parse(localStorage.getItem("storage"));
+
+const transfer = (index, cat) =>{
+    let catteam = JSON.parse(localStorage.getItem("team"))
+    let ix = index-1;
+    if(catteam[ix]==null){
+        catteam[ix] = cat;
+        save(catteam);
+        storage.forEach(item=>{
+            if(item == cat){
+                storage.splice(storage.indexOf(item),1);
+                localStorage.setItem("storage", JSON.stringify(storage));
+            }
+        })
+        location.assign("#");
+    }
+    else{
+        let oldCat = catteam[ix];
+        catteam[ix] = cat;
+        storage.forEach(item=>{
+            if(item == cat){
+                storage[storage.indexOf(item)] = oldCat;
+                localStorage.setItem("storage", JSON.stringify(storage));
+            }
+        })
+        save(catteam);
+        location.assign("#");
+    }
+}
+
+storage.forEach(cat => {
+    const div = document.createElement('div');
+    div.className = 'storage';
+    const dataDiv = document.createElement('div');
+    dataDiv.className = 'data';
+    const imgDiv = document.createElement('div');
+    imgDiv.className = 'image';
+    const image = document.createElement('img');
+    image.src = cat.image;
+    imgDiv.appendChild(image);
+    const name = document.createElement('p');
+    name.className = 'name';
+    name.textContent = cat.name;
+    const hp = document.createElement('p');
+    hp.className = 'hp';
+    hp.textContent = `HP: ${cat.health}/${cat.maxHealth}`;
+    const xp = document.createElement('p');
+    xp.className = 'xp';
+    xp.innerHTML = `<sub>0</sub><meter value="${cat.xp}" max="${cat.maxXP}"></meter><sub>${cat.maxXP}</sub>`;
+    dataDiv.appendChild(imgDiv);
+    dataDiv.appendChild(name);
+    dataDiv.appendChild(hp);
+    dataDiv.appendChild(xp);
+    const useDiv = document.createElement('div');
+    useDiv.className = 'use';
+    for (let i = 1; i <= 6; i++) {
+      const button = document.createElement('button');
+      button.className = `use${i}`;
+      button.textContent = `Use ${i}`;
+      button.addEventListener('click', () => {
+        transfer(i, cat);
+      });
+      useDiv.appendChild(button);
+    }
+    div.appendChild(dataDiv);
+    div.appendChild(useDiv);
+    main.appendChild(div);
+  });
