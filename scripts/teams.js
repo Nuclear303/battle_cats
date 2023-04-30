ff1b6e2f62597688d9138c5ab929a82733fa4197
Nuclear303 <oliwier.kotlowski1@gmail.com>
2023-04-30 19:41:19 +0200

    deleted as it's no longer needed

--- scripts/teams.js
+++
@@ -1,6 +0,0 @@
-import Cat from "./catConstructor.js"
-let cat = new Cat('Cat', 100,8, "None", "cat.png");
-let tank = new Cat('Tank Cat', 400, 2, "None", "tank.png");
-let axe = new Cat('Axe Cat', 300,20,"Red", "axe.png");
-let team;
-export default team = [cat,cat,cat,cat,cat,cat];
