dae71caac0f750b1df4a2ae3b4954196b9b4851e
Nuclear303 <oliwier.kotlowski1@gmail.com>
2023-04-30 22:21:18 +0200

    deleted the mute button

--- scripts/main-menu.js
+++ scripts/main-menu.js
@@ -1,20 +1,5 @@
 import Cat from "./catConstructor.js"
 
-const mainMenu = document.querySelector("audio")
-document.querySelector(".mute_button").addEventListener("click",_=>{
-    switch(document.querySelector(".mute_button").innerText){
-        case "Unmute":{
-            mainMenu.play();
-            mainMenu.loop = true
-            document.querySelector(".mute_button").innerText="Mute"
-
-        };break;
-        case "Mute":{
-            mainMenu.pause();
-            document.querySelector(".mute_button").innerText="Unmute"
-        }
-    }
-})
 
 const images = ["./assets/EoCBackground.webp", "./assets/ItFBackground.webp", "./assets/CotCBackground.webp"]
 document.body.style.backgroundImage = `url(${images[Math.round(Math.random()*2)]})`;
