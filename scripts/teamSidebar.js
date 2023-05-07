11a74683afdd2d261dfbb5b25f77d3dd3c9666ec
Nuclear303 <oliwier.kotlowski1@gmail.com>
2023-05-07 10:44:19 +0200

    Now showing the level of the cat in the sidebar

--- scripts/teamSidebar.js
+++ scripts/teamSidebar.js
@@ -3,7 +3,7 @@ var team = serializer.deserialize(localStorage.getItem("team"));
 for(let i = 0; i < 6; i++){
   if(team[i] != null){
     document.querySelectorAll("div.catimage > img")[i].src = team[i].image;
-    document.querySelectorAll(".cat-name")[i].textContent = team[i].name;
+    document.querySelectorAll(".cat-name")[i].textContent = `${team[i].name} lvl. ${team[i].level}`;
     document.querySelectorAll(".cat-xp")[i].innerHTML = `<sub>0</sub><meter value="${team[i].xp}" max="${team[i].maxXP}"></meter><sub>${team[i].maxXP}</sub>`;
     document.querySelectorAll(".cat-hp")[i].textContent = `HP: ${team[i].health}/${team[i].maxHealth}`;
   }
