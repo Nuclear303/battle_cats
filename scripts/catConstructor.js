229efa1118038d6eacb9570ac32f64853562c71b
Nuclear303 <oliwier.kotlowski1@gmail.com>
2023-05-11 17:08:02 +0200

    added cat evolved forms, changed funcs to arrow ones

--- scripts/catConstructor.js
+++ scripts/catConstructor.js
@@ -1,14 +1,19 @@
 export default class Cat{
-    constructor(catname, base_health, base_damage, attribute, image){
+    constructor(catname, base_health, base_damage, attribute, image, evolvedName, evolvedImage, trueName, trueImage){
         this.name = catname;
         this.level = 1;
         this.attribute = attribute;
         this.health = base_health;
         this.maxHealth = base_health;
         this.damage = base_damage;
-        this.xp = 0;
+        this.xp = 99;
         this.maxXP = 100;
-        this.damageCalc = function(){
+        this.image = `../assets/${image}`;
+        this.evolvedImage = `../assets/${evolvedImage}`;
+        this.evolvedName = evolvedName;
+        this.trueName = trueName;
+        this.trueImage = `../assets/${trueImage}`;
+        this.damageCalc = () =>{
                 if(this.damage >= Math.ceil(this.damage+1.3*Math.log2(this.level))){
                     this.damage++;
                     return
@@ -18,14 +23,16 @@ export default class Cat{
                     return
                 }
         }
-        this.healthCalc =function(){
+        this.healthCalc =() =>{
             this.maxHealth = this.maxHealth+(this.level*10);
             this.health = this.health + (this.level*10);
         }
-        this.maxXPCalc = function(){
+        this.maxXPCalc = () =>{
             this.maxXP = 100+(this.level*50);
         }
-        this.levelUp = function(){
+        
+
+        this.levelUp = () =>{
             this.level++;
             this.damageCalc();
             this.healthCalc();
@@ -33,14 +40,14 @@ export default class Cat{
             this.maxXPCalc();
             this.xp = 0+toomuch;
         }
-        this.ifLevelCap =function(){
+        this.ifLevelCap = () =>{
             if(this.level == 100) return;
             if(this.xp >= this.maxXP){
                 this.levelUp();
                 alert(`${this.name} osiągnął level ${this.level}!`);
             }
         }
-        this.attack = function(enemy){
+        this.attack = (enemy) =>{
             if(enemy.color!=this.attribute){
                 enemy.health-=this.damage
             }
@@ -54,6 +61,7 @@ export default class Cat{
                 return 0;
             }
         }
-        this.image = `../assets/${image}`;
     }
+
 }
+
