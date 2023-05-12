4b341cdbed56f9f9913e283a1185b287e53f069d
Nuclear303 <oliwier.kotlowski1@gmail.com>
2023-05-12 21:09:07 +0200

    stat checker with adding stats

---
+++ scripts/statChecker.js
@@ -0,0 +1,29 @@
+export function AddStat(enemyName){
+  let stats = JSON.parse(localStorage.getItem("stats"));
+  if(stats.hasOwnProperty(enemyName)){
+    stats[enemyName] += 1;
+  }
+  else{
+    stats[enemyName] = 1;
+  }
+  localStorage.setItem("stats", JSON.stringify(stats));
+}
+
+if(location.href.includes("stats/index.html")){
+  let stats = JSON.parse(localStorage.getItem("stats"));
+  const main = document.querySelector("main");
+  if(stats != {}) {
+    let stat = document.createElement("div");
+    stat.className = "stat";
+    stat.innerHTML = `<h1>You haven't killed any enemies yet! Go hunt some baddies!</h1>`;
+    main.appendChild(stat);
+  }
+  else{
+    Object.keys(stats).forEach(enemy=>{
+      let stat = document.createElement("div");
+      stat.className = "stat";
+      stat.innerHTML = `<b>${enemy}</b>: ${stats[enemy]} killed`;
+      main.appendChild(stat);
+    })
+  }
+}
